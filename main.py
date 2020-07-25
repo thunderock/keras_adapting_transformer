693289bb4a0389d77b5b6674cd901a91ef41c5d2
Ashutosh Tiwari <findashutoshtiwari@gmail.com>
2020-07-25 18:10:28 +0530

    starting commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from utils import *
+
+vocab_size = 20000
+max_len = 200
+(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=vocab_size)
+print(len(x_train), "Training sequences")
+print(len(x_val), "Validation sequences")
+x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=max_len)
+x_val = keras.preprocessing.sequence.pad_sequences(x_val, maxlen=max_len)
+
+embed_dim = 32
+num_heads = 2
+ff_dim = 32
+
+inputs = layers.Input(shape=(max_len,))
+embedding_layer = TokenAndPositionEmbedding(max_len, vocab_size, embed_dim)
+x = embedding_layer(inputs)
+transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+x = transformer_block(x)
+x = layers.GlobalAveragePooling1D()(x)
+x = layers.Dropout(0.1)(x)
+x = layers.Dense(20, activation="relu")(x)
+x = layers.Dropout(0.1)(x)
+outputs = layers.Dense(2, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+
+model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+history = model.fit(
+    x_train, y_train, batch_size=32, epochs=2, validation_data=(x_val, y_val)
+)
